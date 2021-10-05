083742e751ebb42b9f600494040c6bfa8a52b883
shellysharma114 <shellysharma346@gmail.com>
2021-10-05 09:31:58 +0530

    modified arrays

--- arrays.cpp
+++ arrays.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 
 int main() {
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
+    
     int arr[5]={7,2,8,5,4} ;
     int mins;
     
